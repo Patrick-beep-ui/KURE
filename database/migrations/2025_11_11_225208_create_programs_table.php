95f39b94b11c557a4794364d0f244a21d326c496
Patrick Solis <patrickresol@gmail.com>
2025-11-11 16:58:55 -0600

    First commit

--- database/migrations/2025_11_11_225208_create_programs_table.php
+++ database/migrations/2025_11_11_225208_create_programs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('programs', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->timestamps(); //
         });
     }
 
