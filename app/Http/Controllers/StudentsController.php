e624cd9e2a51bc809376a227c82eeca3e993eab8
Patrick Solis <patrickresol@gmail.com>
2025-11-14 16:19:33 -0600

    Created students and studentsById endpoint

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
+use Exception;
 
 class StudentsController extends Controller
 {
-    //
+    public function getStudents() {
+        try {
+            $students = Student::with([
+                'program',
+                'contactNumbers' => function($query) {
+                    $query->where('type', 'personal');
+                },
+            ])->get();
+
+
+            return response()->json($students);
+        }
+        catch(Exception $e) {
+            return response()->json(['error' => 'Error fetching students', 'message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function getStudentById($id) {
+        try {
+            $student = Student::with([
+                'program',
+                'contactNumbers',
+                'conditions.condition',
+                'consultations.consultationMedications.medication',
+                'appointments',
+            ])->findOrFail($id);
+
+            return response()->json($student);
+        }
+        catch(Exception $e) {
+            return response()->json(['error' => 'Error fetching student', 'message' => $e->getMessage()], 500);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentsController;
 
-Route::get('/', function () {
+Route::get('/test', function () {
     return response()->json(['message' => 'API is working']);
+});
+
+
+// Students routes
+Route::prefix('students')->group(function () {
+    Route::get('/', [StudentsController::class, 'getStudents']);
+    Route::get('/{id}', [StudentsController::class, 'getStudentById']);
 });
